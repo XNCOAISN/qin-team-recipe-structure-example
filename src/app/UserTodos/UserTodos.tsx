cfb640193142ec75b6ec66088bc6305265a67687
XNCOAISN <end.pgm@gmail.com>
2023-06-04 15:03:49 +0900

    ✨

---
+++ src/app/UserTodos/UserTodos.tsx
@@ -0,0 +1,19 @@
+import { fetchTodos } from "./fetchTodos";
+import { UserTodo } from "./UserTodo";
+
+export const UserTodos = async () => {
+  const todos = await fetchTodos();
+
+  // DEBUG: 3秒待ってから表示する
+  const sleep = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+  await sleep(3000);
+
+  return (
+    <>
+      {todos?.map((todo) => {
+        return <UserTodo key={todo.id} data={todo} />;
+      })}
+    </>
+  );
+};
---
+++ src/features/fetcher.ts
@@ -0,0 +1,12 @@
+export const fetcher = async <T>(
+  pathname: string,
+  revalidateSpan: number = 10
+): Promise<T> => {
+  const hostname = "https://jsonplaceholder.typicode.com";
+
+  const endpoint = hostname + pathname;
+
+  return await fetch(endpoint, { next: { revalidate: revalidateSpan } }).then(
+    (res) => res.json() as T
+  );
+};
